d3b6bc458e6309dbfb6c1d0d504936964c4ed372
M4dn4ss <sura0046@hotmail.com>
2021-08-04 13:12:25 +0300

    TicTacToe

---
+++ Tictactoe/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace Tictactoe
+{
+    /// <summary>
+    /// The type of value a cell in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is an O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is a X
+        /// </summary>
+        Cross
+    }
+}
